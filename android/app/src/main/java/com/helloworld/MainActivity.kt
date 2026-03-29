f36aa86eb77f1fb7cf8b5b2733dcef876e2d9d4c
debnathbisal108 <debnathbisal108@gmail.com>
2026-03-29 21:53:58 +0530

    Update MainActivity.kt

--- android/app/src/main/java/com/helloworld/MainActivity.kt
+++ android/app/src/main/java/com/helloworld/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.presense
 
 import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
-    override fun getMainComponentName(): String = "presense"
+    override fun getMainComponentName(): String = "PreSenseAI"   // Must match index.js exactly
+
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }
