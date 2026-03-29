534f1970cbe26a9c1c26c48f4f04054dc2763d6c
debnathbisal108 <debnathbisal108@gmail.com>
2026-03-29 15:50:23 +0530

    Update MainActivity.kt

--- android/app/src/main/java/com/helloworld/MainActivity.kt
+++ android/app/src/main/java/com/helloworld/MainActivity.kt
@@ -1,34 +1,8 @@
-// package com.helloworld
+package com.presense
 
-// import com.facebook.react.ReactActivity
-// import com.facebook.react.ReactActivityDelegate
-// import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
-// import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.facebook.react.ReactActivity
 
-// class MainActivity : ReactActivity() {
+class MainActivity : ReactActivity() {
 
-//   /**
-//    * Returns the name of the main component registered from JavaScript. This is used to schedule
-//    * rendering of the component.
-//    */
-//   override fun getMainComponentName(): String = "HelloWorld"
-
-//   /**
-//    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-//    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-//    */
-//   override fun createReactActivityDelegate(): ReactActivityDelegate =
-//       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
-// }
-
-
-package com.helloworld
-
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    override fun getMainComponentName(): String = "presense"
 }
